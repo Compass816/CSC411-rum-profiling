1fa5e389fb8fc0fc47fdc07c0b76b2281b47b432
Ben Keeler <71103393+benkeeler12@users.noreply.github.com>
2023-12-07 20:57:11 -0500

    Update main.rs
    adding vector memory representation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use rum::machine;
 use std::env;
+use rum::memory::Memory;
 
 fn main() {
     let filename = env::args().nth(1).expect("Usage: rum progname");
-    let instructions = machine::boot(&filename);
-    machine::run(instructions);
+    let instructions = rum::machine::boot(&filename);
+    let mut memory = Memory::new(instructions);
+    rum::machine::run(&mut memory);
 }
